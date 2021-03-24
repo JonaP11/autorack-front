3c5cc55f6e7da236616333fc898161732e00861f
Joziah Mays <jmays2@wisc.edu>
2021-03-24 01:20:13 -0500

    Fixed error with dysfunctional private and public routes

--- src/components/auth/PrivateRoute.tsx
+++ src/components/auth/PrivateRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {Redirect, Route, RouteProps} from 'react-router-dom';
+import {Route, Redirect, RouteProps} from 'react-router-dom';
 
 import AppPaths from '../../const/paths';
 import {RootState} from '../../state/store';
@@ -11,14 +11,9 @@ type PrivateRouteProps = RouteProps & RouteComponentProps;
 export const PrivateRoute = ({component: Component, ...rest}: PrivateRouteProps) => {
   const {user} = useSelector((state: RootState) => state.auth);
 
-  if (!user) {
-    return <Redirect to={AppPaths.SIGN_IN}/>;
-  }
-
   return (
     <Route
-      render={(props) => <Component {...props} />}
       {...rest}
-    />
+      render={(props) => user ? <Component {...props} /> : <Redirect to={AppPaths.SIGN_IN} />} />
   );
 };
--- src/components/auth/PublicRoute.tsx
+++ src/components/auth/PublicRoute.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useSelector} from 'react-redux';
-import {Redirect, Route, RouteProps} from 'react-router-dom';
-import AppPaths from '../../const/paths';
+import {Route, Redirect, RouteProps} from 'react-router-dom';
 
+import AppPaths from '../../const/paths';
 import {RootState} from '../../state/store';
 import {RouteComponentProps} from './props';
 
@@ -11,15 +11,10 @@ type PublicRouteProps = RouteProps & RouteComponentProps;
 export const PublicRoute = ({component: Component, ...rest}: PublicRouteProps) => {
   const {user} = useSelector((state: RootState) => state.auth);
 
-  if (user) {
-    // FIXME: Testing placeholder, to be removed
-    return <Redirect to={AppPaths.CALC}/>;
-  }
-
   return (
     <Route
-      render={(props) => <Component {...props} />}
       {...rest}
+      render={(props) => !user ? <Component {...props} /> : <Redirect to={AppPaths.CALC} />}
     />
   );
 };
