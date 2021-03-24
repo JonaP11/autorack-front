aa192bd4113366d49a175eb2d3df87b35ffdc0d7
RaenonX <raenonx0710@gmail.com>
2021-03-23 20:43:30 -0500

    Fixed provider not used in CI
    Signed-off-by: RaenonX <raenonx0710@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {store, persistor} from './store';
-import {PersistGate} from 'redux-persist/integration/react';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <App/>
-    </PersistGate>
-  </Provider>,
+  <App/>,
   document.getElementById('root'),
 );
 
